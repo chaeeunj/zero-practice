d7dc0215e90f01caec8159305df9b10e582f01ff
chaeeunj <official2930@naver.com>
2023-05-25 02:04:29 +0900

    자바스크립트 데이터 포맷(JSON)

--- app.js
+++ app.js
@@ -1,49 +1,18 @@
-// - 웹 스토리지
+// JSON(JavaScript Object Notation) : 자바스크립트에서 사용할 용도로 만들어진 데이터 포맷
 
-// 브라우저에 저장되는 키-값 쌍
-// 1. 로컬 스토리지 : 브라우저 종료 시에도 유지되는 데이터
-// 2. 세션 스토리지 : 새로 고침 시에도 유지되는 데이터(같은 탭 내에서만)
+// JSON.parse(JSON string) : JSON을 객체로 변환
+// JSON.stringify(객체 변수) : 객체를 JSON으로 변환
 
-// 쿠키와 다른 점
-// 1. 요청 헤더에 포함되지 않음
-// 2. 클라이언트에서만 조정 가능
-// 3. 해당 URL에서만 접근 가능
+//  ! 함수나 undefined 값을 가진 프로퍼티는 무시함
 
-// 일정 시간동안만 데이터가 유지되길 바라면 쿠키를 사용
+// const obj = {
+//   "students" : [
+//     {
+//       "name": "cali",
+//       "age" : 20,
+//     }
+//   ]
+// }
 
-// --------------------------------------------------
-
-// 웹 스토리지 쓰기
-
-// 1. 로컬 스토리지
-// (1) localStorage.setItem('key', value);
-// ex) localStorage.setItem('name', 'jce');
-// (2) localStorage.key = value;
-
-// 2. 세션 스토리지
-// (1) sessionStorage.setItem('key', value);
-// (2) sessionStorage.key = value;
-
-// -------------------------------------------------
-
-// 웹 스토리지 접근
-
-// 1. 로컬 스토리지
-// (1) localStorage.getItem('key');
-// (2) localStorage.key
-
-// 2. 세션 스토리지
-// (1) sessionStorage.getItem('key');
-// (2) sessionStorage[key];
-
-// -------------------------------------------------
-
-// 웹 스토리지 삭제
-
-// 1. 로컬 스토리지
-// (1) localStorage.removeItem('key');
-// (2) localStorage.clear();
-
-// 2. 세션 스토리지
-// (1) sessionStorage.removeItem('key');
-// (2) sessionStorage.clear();
+const jsonData = JSON.stringify(obj); // 객체가 JSON으로 변환됨
+JSON.parse(jsonData); // JSON이 객체로 변환됨
