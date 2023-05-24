d0cebb9b9bf3b380479edd61e6f315849fe04b8f
chaeeunj <official2930@naver.com>
2023-05-24 16:32:37 +0900

    자바스크립트 네트워크 요청

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const A = (callback) => {
-  const a = 'A 함수 실행 후 생기는 데이터';
-  console.log('A 함수 실행');
-  callback(a);
-};
+fetch('./sample.json').then(async (result) => {
+  console.log('result', result);
+  if (result.ok) {
+    const data = await result.json();
+    console.log(data);
+  }
+});
 
-const B = (a) => {
-  console.log(`${a}를 이용하는 B`);
-};
-
-A(B);
+fetch('https://ohou.se')
+  .then((res) => {
+    console.log('success', res);
+  })
+  .then((res) => {
+    console.log('success', res);
+  })
+  .catch((err) => {
+    console.log('error', err);
+  });
